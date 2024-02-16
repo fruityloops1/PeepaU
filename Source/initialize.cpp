9ea3741fe2316960d7386d3bd7f6d4719ac47b65
STUPID Modder <stupidmoddersoup@gmail.com>
2024-02-16 03:45:29 -0300

    fixed cemu 2.0 support

--- Source/initialize.cpp
+++ Source/initialize.cpp
@@ -14,19 +14,31 @@ OsSpecifics osSpecifics;
 extern u32 BLOSDynLoad_Acquire;
 extern u32 BOSDynLoad_FindExport;
 
+uintptr_t extractAddrFromInstr(const u32* instr)
+{
+    uintptr_t ret = *instr & 0x03FFFFFCU;
+
+    if (!(*instr & 2))
+    {
+        // sign extend offset
+        if (ret & 0x02000000U)
+            ret |= 0xFE000000U;
+
+        // make relative
+        ret += (uintptr_t)instr;
+    }
+
+    return ret;
+}
+
 extern "C" void initialize()
 {
     for (int i = 0; _ctors[i]; i++)
         (*_ctors[i])();
 
     // Set OSDynLoad_Acquire and OSDynLoad_FindExport
-    OS_SPECIFICS->addr_OSDynLoad_Acquire = (u32)(BLOSDynLoad_Acquire & 0x03FFFFFC);
-    OS_SPECIFICS->addr_OSDynLoad_FindExport = (u32)(BOSDynLoad_FindExport & 0x03FFFFFC);
-
-    if (!(BLOSDynLoad_Acquire & 2))
-        OS_SPECIFICS->addr_OSDynLoad_Acquire += (u32)&BLOSDynLoad_Acquire;
-    if (!(BOSDynLoad_FindExport & 2))
-        OS_SPECIFICS->addr_OSDynLoad_FindExport += (u32)&BOSDynLoad_FindExport;
+    OS_SPECIFICS->addr_OSDynLoad_Acquire = extractAddrFromInstr(&BLOSDynLoad_Acquire);
+    OS_SPECIFICS->addr_OSDynLoad_FindExport = extractAddrFromInstr(&BOSDynLoad_FindExport);
 
     InitOSFunctionPointers();
     InitGX2FunctionPointers();
